77e3aa786aa6606781407beab48256c3f821aaf8
faisalbawono <38713099+faisalbawono@users.noreply.github.com>
2019-12-14 23:25:45 +0700

    last

--- application/views/home.php
+++ application/views/home.php
@@ -1,65 +1,299 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-	<title>Lapor.Itera</title>
-  <link rel="stylesheet" type="text/css" href="<?php echo base_url('asset/style.css') ?>">
+<title>Page Title</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+* {
+  box-sizing: border-box;
+}
+
+/* Style the body */
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  margin: 0;
+}
+
+/* ini header */
+
+
+}
+
+/* Increase the font size of the heading */
+.header h1 {
+  font-size: 40px;
+}
+
+/* Style the top navigation bar */
+.navbar {
+  overflow: hidden;
+  background-color: #333;
+}
+
+/* Style the navigation bar links */
+.navbar a {
+  float: left;
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 20px;
+  text-decoration: none;
+}
+
+/* Right-aligned link */
+.navbar a.right {
+  float: right;
+}
+
+/* Change color on hover */
+.navbar a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+/* Column container */
+.row {
+  display: -ms-flexbox; /* IE10 */
+  display: flex;
+  -ms-flex-wrap: wrap; /* IE10 */
+  flex-wrap: wrap;
+}
+
+/* Create two unequal columns that sits next to each other */
+/* Sidebar/left column */
+.side {
+  -ms-flex: 30%; /* IE10 */
+  flex: 30%;
+  background-color: #f1f1f1;
+  padding: 20px;
+}
+
+/* Main column */
+.main {
+  -ms-flex: 70%; /* IE10 */
+  flex: 70%;
+  background-color: white;
+  padding: 20px;
+}
+
+/* Fake image, just for this example */
+.fakeimg {
+  background-color: #aaa;
+  width: 100%;
+  padding: 20px;
+}
+
+.container {
+  position: relative;
+  width: 100%;
+  max-width: 400px;
+}
+
+.container img {
+  width: 100%;
+  height: auto;
+}
+
+.container .btn {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  -ms-transform: translate(-50%, -50%);
+  background-color: #555;
+  color: white;
+  font-size: 16px;
+  padding: 12px 24px;
+  border: none;
+  cursor: pointer;
+  border-radius: 5px;
+  text-align: center;
+}
+
+.container .btn:hover {
+  background-color: black;
+}
+
+
+/* Footer */
+.footer {
+  padding: 5px;
+  text-align: center;
+  background: white;
+}
+
+/* Responsive layout - when the screen is less than 700px wide, make the two columns stack on top of each other instead of next to each other */
+@media screen and (max-width: 700px) {
+  .row {
+    flex-direction: column;
+  }
+}
+
+/* Responsive layout - when the screen is less than 400px wide, make the navigation links stack on top of each other instead of next to each other */
+@media screen and (max-width: 100px) {
+  .navbar a {
+    float: none;
+    width: 100%;
+  }
+}
+
+/* menu bar */
+
+.topnav {
+  overflow: hidden;
+  background-color: #e9e9e9;
+}
+
+.topnav a {
+  float: left;
+  display: block;
+  color: black;
+  text-align: center;
+  padding: 30px 28px;
+  text-decoration: none;
+  font-size: 17px;
+}
+
+.topnav a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+.topnav a.active {
+  background-color: #2196F3;
+  color: white;
+}
+
+.topnav .search-container {
+  float: right;
+}
+
+.topnav input[type=text] {
+  padding: 6px;
+  margin-top: 25px;
+  font-size: 17px;
+  border: none;
+}
+
+.topnav .search-container button {
+  float: right;
+  padding: 6px 10px;
+  margin-top: 8px;
+  margin-right: 16px;
+  background: #ddd;
+  font-size: 17px;
+  border: none;
+  cursor: pointer;
+}
+
+.topnav .search-container button:hover {
+  background: #ccc;
+}
+
+@media screen and (max-width: 600px) {
+  .topnav .search-container {
+    float: none;
+  }
+  .topnav a, .topnav input[type=text], .topnav .search-container button {
+    float: none;
+    display: block;
+    text-align: left;
+    width: 100%;
+    margin: 25px;
+    padding: 14px;
+  }
+  .topnav input[type=text] {
+    border: 10px solid #ccc;
+  }
+}
+
+
+
+.btn:hover {opacity: 0.6}
+
+.container {
+  position: relative;
+  width: 100%;
+  max-width: 400px;
+}
+
+.container img {
+  width: 100%;
+  height: auto;
+}
+
+.container .btn {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  -ms-transform: translate(-50%, -50%);
+  background-color: #555;
+  color: red;
+  font-size: 16px;
+  padding: 12px 24px;
+  border: none;
+  cursor: pointer;
+  border-radius: 5px;
+  text-align: center;
+}
+
+.container .btn:hover {
+  background-color: black;
+}
+
+</style>
 </head>
+
+
+
 
 <body>
 
-<div id="header">
-  <h1>SIMPLE Laporan</h1>
+<center>
+  <div class="container">
+  <img src="aa.png"  alt="Snow" style="width:100%">
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <button class="btn"><a href="http://localhost/pengaduan/tambah">lapor</button>
 </div>
 
-<div id="content">
 
-  <center>
-    <form >
-        <div class="cari" style="margin-top: 50px;">
-          <input type="search" name="search" placeholder="Cari...">
-          <input type="submit" value="cari" name="cari">
-        </div>
+</center>
+
+<div class="topnav">
+  <a class="active" href="#home">Home</a>
+  <a href="http://localhost/pengaduan/laporan">laporan</a>
+  <a href="http://localhost/pengaduan/ubah">ubah</a>
+  <div class="search-container">
+    <form action="/action_page.php">
+      <input type="text" placeholder="Search.." name="search">
+      <button class="btn">find</button>
     </form>
-  </center>
-
-<br>
-
-  <a href="<?php echo base_url('buat') ?>"><h3>Buat Laporan/Komentar</h3></a>
-
-<br>
-<br>
-
-  <legend>Laporan/Komentar Terakhir</legend>
-  <hr>
-  
-  <?php foreach ($berita as $berita) {?>
-  
-  <div class="Artikel">
-    <p class="home"><?php echo $berita->isi ?></p><br>
-
-    <ul>
-    <li>Lampiran: <?php echo $berita->file ?></a></li>
-    <li>Waktu: <?php echo $berita->waktu ?></li>
-    <li style="float: right;"><a href="<?php echo base_url('home/read/'.$berita->id_laporan) ?>">lihat selengkapnya></a></li>
-    </ul><br>
-    <br>
-    
-    <hr>
-    <?php } ?>
   </div>
+</div>
 <br>
 <br>
-      <div class="page">
-        <?php if (isset($page) && $total > $limit) {
-               $page;
-        }?>
-        </div>
-
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<div class="footer">
+  <h2>[copyright] teknik informatika (Faisal bawono  | Yuwanda indra )</h2>
 </div>
 
-<div id="footer">
-  <p class="teksputer">Copyright &copy 2019 - Nayaka Pitra Raditya dan Reki Selianta</p>
-</div>
 
 </body>
 </html>
